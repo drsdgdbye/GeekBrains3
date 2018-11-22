9d9ec36298721a4510e1b1e9e5dd53f1ce2245fd
drsdgdby <nolifeirl@2ch.hk>
2018-11-22 15:16:44 +0300

    .

--- src/Lesson_1_3/Box.java
+++ src/Lesson_1_3/Box.java
@@ -3,17 +3,17 @@ package Lesson_1_3;
 import java.util.ArrayList;
 
 class Box<T extends Fruit> {
-    private ArrayList<T> box;
-    private float weight;
 
-    void addFruit(ArrayList<T> box, T fruit){
-        box.add(fruit);
+    private ArrayList<T> box;
+
+    void addFruit(T fruit){
+        this.box.add(fruit);
     }
 
-    void intersperseFruits(ArrayList<T> box, ArrayList<T> anotherBox){
+    void intersperseFruits(ArrayList<T> anotherBox){
         for (int i = 0; i < box.size(); i++) {
-            anotherBox.add(box.get(0));
-            box.remove(0);
+            anotherBox.add(this.box.get(0));
+            this.box.remove(0);
         }
     }
 
@@ -25,5 +25,13 @@ class Box<T extends Fruit> {
        return this.getWeight() == anotherBox.getWeight();
     }
 
+    public static void main(String[] args) {
+        Box<Apple> box = new Box<>();
+        box.addFruit(new Apple());
+        Box<Apple> box1 = new Box<>();
+        box.intersperseFruits(new ArrayList<>());
+        box.compare(box1);
+
+    }
 
 }

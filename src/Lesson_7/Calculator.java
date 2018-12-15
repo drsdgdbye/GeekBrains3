b1730d09db714f28b436321d67086a03faa82dc1
drsdgdby <nolifeirl@2ch.hk>
2018-12-15 07:07:20 +0300

    .

---
+++ src/Lesson_7/Calculator.java
@@ -0,0 +1,13 @@
+package Lesson_7;
+
+public class Calculator {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+}
